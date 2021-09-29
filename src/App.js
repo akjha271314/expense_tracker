aee21b2655effc5a08e1ea68d8eaef5892438435
akjha271314 <akjha271314@gmail.com>
2021-09-29 13:39:35 +0530

    Categories added, date format changed and chartData added for both income and expense

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
 
     return (
         <div>
-            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: "20vh" }}>
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: "100vh" }}>
                 <Grid item xs={12} sm={4}>
                     <Details title="Income" />
                 </Grid>
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 
 import useStyles from './styles';
+import Form from './Form/Form';
+import List from './List/List';
 
 const Main = () => {
     const classes = useStyles();
@@ -16,12 +18,12 @@ const Main = () => {
                     Try saying : Add income for $100 in category Salary for monday...
                 </Typography>
                 <Divider />
-                {/* Form */}
+                <Form />
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        {/* <List /> */}
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
