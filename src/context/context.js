d3baa454477b35b4952ec144305833ef735f54d8
akjha271314 <akjha271314@gmail.com>
2021-10-01 00:59:16 +0530

    Added speechly features and mobile responsive features (last commit)

--- src/context/context.js
+++ src/context/context.js
@@ -2,7 +2,7 @@ import React, { useReducer, createContext } from 'react';
 
 import contextReducer from './contextReducer';
 
-const initialState = [];
+const initialState = JSON.parse(localStorage.getItem('transactions')) || [{"amount":200,"category":"Extra income","type":"Income","date":"2021-09-30","id":"6d877b46-51aa-4b0c-b738-187e0107ab0d"},{"amount":80,"category":"Deposits","type":"Income","date":"2021-09-30","id":"7ca4773c-72b9-44da-a862-bd1bc873bae3"},{"amount":75,"category":"Food","type":"Expense","date":"2021-09-30","id":"bcbbd8f8-3c89-46b2-a60b-766de8291354"},{"amount":150,"category":"Clothes","type":"Expense","date":"2021-09-30","id":"f77b4a9a-1fbf-4d7a-82cf-d0bcdbfbdaf3"}];
 
 export const ExpenseTrackerContext = createContext(initialState);
 
@@ -13,12 +13,17 @@ export const Provider = ({ children }) => {
     const deleteTransaction = (id) => dispatch({ type: 'DELETE_TRANSACTION', payload: id });
     const addTransaction = (transaction) => dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
 
+    const balance = transactions.reduce((acc, currVal) => {
+        return (currVal.type === 'Expense' ? acc - currVal.amount : acc + currVal.amount);
+    }, 0);
+
 
     return (
         <ExpenseTrackerContext.Provider value={{ 
             deleteTransaction,
             addTransaction,
-            transactions
+            transactions,
+            balance
          }}>
             {children}
         </ExpenseTrackerContext.Provider>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { SpeechProvider } from '@speechly/react-client';
 
 import { Provider } from './context/context';
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
-    <Provider>
-        <App />
-    </Provider>,
-document.getElementById('root')
+    <SpeechProvider appId="8fa04513-110c-462e-950b-5a336a8b339f" language="en-US">
+        <Provider>
+            <App />
+        </Provider>
+    </SpeechProvider>,
+    document.getElementById('root')
 );
